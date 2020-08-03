6b60c18d584849ec5766ea5d266be2772f0ea277
phatvt2 <phatvt2@vng.com.vn>
2020-08-04 00:28:17 +0700

    [SplashScreen] Add navigation component

---
+++ app/src/main/java/vn/tiki/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package vn.tiki.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vn.tiki.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
